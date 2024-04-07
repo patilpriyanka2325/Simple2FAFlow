33c97c8308a8b2cd4222e77f3af5488672633bd3
patilpriyanka2325 <patilpriyanka2325@gmail.com>
2024-04-07 16:11:55 +0530

    Next buttons visibility is managed

--- Simple2FAFlow/ViewModel/NavigationVM.cs
+++ Simple2FAFlow/ViewModel/NavigationVM.cs
@@ -1,15 +1,32 @@
 ﻿using Simple2FAFlow.Core;
+using System.Windows;
 using System.Windows.Input;
 
 namespace Simple2FAFlow.ViewModel
 {
-    public class NavigationVM :ViewModelBase
+    public class NavigationVM : ViewModelBase
     {
+        private Visibility _isButtonVisible;
+        public Visibility IsButtonVisible
+        {
+            get => _isButtonVisible;
+            set
+            {
+                _isButtonVisible = value;
+                OnPropertyChanged(nameof(IsButtonVisible));
+            }
+        }
+
         private object _currentView;
         public object CurrentView
         {
             get { return _currentView; }
-            set { _currentView = value; OnPropertyChanged(); }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+                IsButtonVisible = (_currentView is HomeVM) ? Visibility.Visible : Visibility.Collapsed;
+            }
         }
 
         public ICommand HomeCommand { get; set; }
